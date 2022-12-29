f81edc6a6221d6a8f2a475c67cffb59ec74b1c34
ammar-2058 <chammarfarooq0009@gmail.com>
2022-12-29 15:08:22 +0400

    Ammar

---
+++ database/database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `ammar` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(50) NOT NULL,
+ `email` varchar(50) NOT NULL,
+ `password` varchar(50) NOT NULL,
+ PRIMARY KEY (`id`)
+ );
+
+INSERT INTO ammar (name,email,password) VALUES
+("ammar","farooq@gmail.com","123")
+
+subhan","farooq@gmail.com","123");
